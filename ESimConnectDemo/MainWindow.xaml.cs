459d67a8b2bd6d06246ecb3e7c7fa6074f2606c8
Marek Vajgl <engin@seznam.cz>
2024-05-08 12:00:47 +0200

    ESimConnect WPF app shows units for simvars

--- ESimConnectDemo/MainWindow.xaml.cs
+++ ESimConnectDemo/MainWindow.xaml.cs
@@ -12,149 +12,154 @@ using System.Windows.Shapes;
 
 namespace ESimConnectDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Model Model { get; set; } = new();
+    private readonly ESimConnect.ESimConnect simCon = new();
+
+    public MainWindow()
     {
-        private Model Model { get; set; } = new();
-        private readonly ESimConnect.ESimConnect simCon = new();
+      InitializeComponent();
+      this.DataContext = Model;
+      this.cmbUnit.SelectedIndex = 0;
 
-        public MainWindow()
+      this.simCon.Connected += SimCon_Connected;
+      this.simCon.Disconnected += SimCon_Disconnected;
+      this.simCon.ThrowsException += SimCon_ThrowsException;
+      this.simCon.DataReceived += SimCon_DataReceived;
+      this.simCon.SystemEventInvoked += SimCon_EventInvoked;
+    }
+
+    private void SimCon_EventInvoked(ESimConnect.ESimConnect _, ESimConnect.ESimConnect.ESimConnectSystemEventInvokedEventArgs e)
+    {
+      Model.SimVarEvent? se = Model.Events.FirstOrDefault(q => q.EventId == e.EventId);
+      if (se == null) return;
+      se.LastInvoked = DateTime.Now;
+      Model.FiredEvents.Insert(0, $"System event '{se.Name}' invoked at '{se.LastInvoked}' with argument '{e.Value}'.");
+    }
+
+    private void SimCon_DataReceived(ESimConnect.ESimConnect _, ESimConnect.ESimConnect.ESimConnectDataReceivedEventArgs e)
+    {
+      Model.SimVarValue? svv = Model.Values.FirstOrDefault(q => q.RequestId == e.RequestId);
+      if (svv == null) return;
+      svv.Value = (double)e.Data;
+    }
+
+    private void SimCon_ThrowsException(ESimConnect.ESimConnect _, ESimConnect.SimConnectException ex)
+    {
+      MessageBox.Show($"SimCon exception: {ex}", "SimCon throw exception", MessageBoxButton.OK, MessageBoxImage.Error);
+      btnConnectDisconnect.IsEnabled = true;
+    }
+
+    private void SimCon_Disconnected(ESimConnect.ESimConnect _)
+    {
+      Model.ConnectionStatus = Model.EConnectionStatus.DISCONNECTED;
+      btnConnectDisconnect.IsEnabled = true;
+    }
+
+    private void SimCon_Connected(ESimConnect.ESimConnect _)
+    {
+      Model.ConnectionStatus = Model.EConnectionStatus.CONNECTED;
+      btnConnectDisconnect.IsEnabled = true;
+    }
+
+    private void btnConnectDisconnect_Click(object sender, RoutedEventArgs e)
+    {
+      btnConnectDisconnect.IsEnabled = false;
+      if (simCon.IsOpened)
+        simCon.Close();
+      else
+        try
         {
-            InitializeComponent();
-            this.DataContext = Model;
-
-            this.simCon.Connected += SimCon_Connected;
-            this.simCon.Disconnected += SimCon_Disconnected;
-            this.simCon.ThrowsException += SimCon_ThrowsException;
-            this.simCon.DataReceived += SimCon_DataReceived;
-            this.simCon.SystemEventInvoked += SimCon_EventInvoked;
+          simCon.Open();
         }
-
-        private void SimCon_EventInvoked(ESimConnect.ESimConnect _, ESimConnect.ESimConnect.ESimConnectSystemEventInvokedEventArgs e)
+        catch (ESimConnectException ex)
         {
-            Model.SimVarEvent? se = Model.Events.FirstOrDefault(q => q.EventId == e.EventId);
-            if (se == null) return;
-            se.LastInvoked = DateTime.Now;
-            Model.FiredEvents.Insert(0, $"System event '{se.Name}' invoked at '{se.LastInvoked}' with argument '{e.Value}'.");
-        }
-
-        private void SimCon_DataReceived(ESimConnect.ESimConnect _, ESimConnect.ESimConnect.ESimConnectDataReceivedEventArgs e)
-        {
-            Model.SimVarValue? svv = Model.Values.FirstOrDefault(q => q.RequestId == e.RequestId);
-            if (svv == null) return;
-            svv.Value = (double)e.Data;
-        }
-
-        private void SimCon_ThrowsException(ESimConnect.ESimConnect _, ESimConnect.SimConnectException ex)
-        {
-            MessageBox.Show($"SimCon exception: {ex}", "SimCon throw exception", MessageBoxButton.OK, MessageBoxImage.Error);
-            btnConnectDisconnect.IsEnabled = true;
-        }
-
-        private void SimCon_Disconnected(ESimConnect.ESimConnect _)
-        {
-            Model.ConnectionStatus = Model.EConnectionStatus.DISCONNECTED;
-            btnConnectDisconnect.IsEnabled = true;
-        }
-
-        private void SimCon_Connected(ESimConnect.ESimConnect _)
-        {
-            Model.ConnectionStatus = Model.EConnectionStatus.CONNECTED;
-            btnConnectDisconnect.IsEnabled = true;
-        }
-
-        private void btnConnectDisconnect_Click(object sender, RoutedEventArgs e)
-        {
-            btnConnectDisconnect.IsEnabled = false;
-            if (simCon.IsOpened)
-                simCon.Close();
-            else
-                try
-                {
-                    simCon.Open();
-                }
-                catch (ESimConnectException ex)
-                {
-                    MessageBox.Show("Failed to connect: " + ex.Message, "Error");
-                    btnConnectDisconnect.IsEnabled = true;
-                }
-        }
-
-        private void btnValueSimVarAdd_Click(object sender, RoutedEventArgs e)
-        {
-            if (simCon.IsOpened == false)
-            {
-                MessageBox.Show("SimCon not connected.", "Error");
-                return;
-            }
-
-            string simVar = txtValueSimVar.Text.Trim().ToUpper();
-            Model.SimVarValue svv = new()
-            {
-                Name = simVar,
-                Value = 0,
-                Period = SimConnectPeriod.SECOND,
-                OnlyOnChange = true,
-                TypeId = simCon.Values.Register<double>(simVar)
-            };
-            svv.RequestId = simCon.Values.RequestRepeatedly(svv.TypeId, svv.Period, sendOnlyOnChange: svv.OnlyOnChange);
-
-            svv.UpdateRequest += Svv_UpdateRequest;
-
-            this.Model.Values.Add(svv);
-
-            txtValueSimVar.Text = "";
-            txtValueSimVar.Focus();
-        }
-
-        private void Svv_UpdateRequest(Model.SimVarValue sender)
-        {
-            simCon.Values.RequestRepeatedly(sender.RequestId, sender.TypeId, sender.Period, sender.OnlyOnChange, skipBetweenFrames: sender.FrameSkip);
-        }
-
-        private void btnValueDelete_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            Model.SimVarValue svv = (Model.SimVarValue)btn.Tag;
-            Model.Values.Remove(svv);
-        }
-
-        private void btnAddSystemEvent_Click(object sender, RoutedEventArgs e)
-        {
-            if (simCon.IsOpened == false)
-            {
-                MessageBox.Show("SimCon not connected.", "Error");
-                return;
-            }
-
-            string eventName = cmbSystemEvent.Text;
-            EventId eid = simCon.SystemEvents.Register(eventName);
-            Model.SimVarEvent se = new()
-            {
-                Name = eventName,
-                EventId = eid
-            };
-            this.Model.Events.Add(se);
-
-            cmbSystemEvent.SelectedIndex = -1;
-        }
-
-        private void btnDeleteSystemEvent_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            Model.SimVarEvent? se = (Model.SimVarEvent)btn.Tag;
-            if (se == null) return;
-            simCon.SystemEvents.Unregister(se.EventId);
-            Model.Events.Remove(se);
-        }
-
-        private void btnSimVarSetValue_Click(object sender, RoutedEventArgs e)
-        {
-            Button btn = (Button)sender;
-            Model.SimVarValue svv = (Model.SimVarValue)btn.Tag;
-            simCon.Values.Send<double>(svv.TypeId, svv.ValueToSet);
+          MessageBox.Show("Failed to connect: " + ex.Message, "Error");
+          btnConnectDisconnect.IsEnabled = true;
         }
     }
+
+    private void btnValueSimVarAdd_Click(object sender, RoutedEventArgs e)
+    {
+      if (simCon.IsOpened == false)
+      {
+        MessageBox.Show("SimCon not connected.", "Error");
+        return;
+      }
+
+      string simVar = txtValueSimVar.Text.Trim().ToUpper();
+      Model.ComboBoxDataItem simUnitItem = cmbUnit.SelectedItem as Model.ComboBoxDataItem;
+      string unitName = simUnitItem?.Label ?? "";
+      string unitSimUnit = simUnitItem?.Value ?? "Number"; // "Number" is default unit value
+      Model.SimVarValue svv = new()
+      {
+        Name = simVar,
+        UnitName = unitName,
+        Value = 0,
+        Period = SimConnectPeriod.SECOND,
+        OnlyOnChange = true,
+        TypeId = simCon.Values.Register<double>(simVar, unit: unitSimUnit)
+      };
+      svv.RequestId = simCon.Values.RequestRepeatedly(svv.TypeId, svv.Period, sendOnlyOnChange: svv.OnlyOnChange);
+
+      svv.UpdateRequest += Svv_UpdateRequest;
+
+      this.Model.Values.Add(svv);
+
+      txtValueSimVar.Text = "";
+      txtValueSimVar.Focus();
+    }
+
+    private void Svv_UpdateRequest(Model.SimVarValue sender)
+    {
+      simCon.Values.RequestRepeatedly(sender.RequestId, sender.TypeId, sender.Period, sender.OnlyOnChange, skipBetweenFrames: sender.FrameSkip);
+    }
+
+    private void btnValueDelete_Click(object sender, RoutedEventArgs e)
+    {
+      Button btn = (Button)sender;
+      Model.SimVarValue svv = (Model.SimVarValue)btn.Tag;
+      Model.Values.Remove(svv);
+    }
+
+    private void btnAddSystemEvent_Click(object sender, RoutedEventArgs e)
+    {
+      if (simCon.IsOpened == false)
+      {
+        MessageBox.Show("SimCon not connected.", "Error");
+        return;
+      }
+
+      string eventName = cmbSystemEvent.Text;
+      EventId eid = simCon.SystemEvents.Register(eventName);
+      Model.SimVarEvent se = new()
+      {
+        Name = eventName,
+        EventId = eid
+      };
+      this.Model.Events.Add(se);
+
+      cmbSystemEvent.SelectedIndex = -1;
+    }
+
+    private void btnDeleteSystemEvent_Click(object sender, RoutedEventArgs e)
+    {
+      Button btn = (Button)sender;
+      Model.SimVarEvent? se = (Model.SimVarEvent)btn.Tag;
+      if (se == null) return;
+      simCon.SystemEvents.Unregister(se.EventId);
+      Model.Events.Remove(se);
+    }
+
+    private void btnSimVarSetValue_Click(object sender, RoutedEventArgs e)
+    {
+      Button btn = (Button)sender;
+      Model.SimVarValue svv = (Model.SimVarValue)btn.Tag;
+      simCon.Values.Send<double>(svv.TypeId, svv.ValueToSet);
+    }
+  }
 }
--- ESimConnectDemo/Model.cs
+++ ESimConnectDemo/Model.cs
@@ -1,4 +1,5 @@
 ﻿using ESimConnect;
+using ESimConnect.Definitions;
 using ESystem.Miscelaneous;
 using System;
 using System.Collections.Generic;
@@ -6,150 +7,190 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ESimConnectDemo
 {
-    class Model : ESystem.Miscelaneous.NotifyPropertyChanged
+  class Model : ESystem.Miscelaneous.NotifyPropertyChanged
+  {
+    public enum EConnectionStatus
     {
-        public enum EConnectionStatus
-        {
-            CONNECTED,
-            DISCONNECTED
-        }
-
-        public class SimVarValue : NotifyPropertyChanged
-        {
-            public event Action<SimVarValue>? UpdateRequest;
-            public TypeId TypeId { get; set; }
-            public RequestId RequestId { get; set; }
-
-            public string Name
-            {
-                get => base.GetProperty<string>(nameof(Name))!;
-                set => base.UpdateProperty(nameof(Name), value);
-            }
-
-            public double Value
-            {
-                get => base.GetProperty<double>(nameof(Value));
-                set
-                {
-                    base.UpdateProperty(nameof(Value), value);
-                    var now = DateTime.Now;
-                    this.LastUpdateInterval = now - this.LastUpdateTime;
-                    this.LastUpdateTime = now;
-                }
-            }
-
-            public ESimConnect.SimConnectPeriod Period
-            {
-                get => base.GetProperty<ESimConnect.SimConnectPeriod>(nameof(Period));
-                set
-                {
-                    base.UpdateProperty(nameof(Period), value);
-                    this.UpdateRequest?.Invoke(this);
-                }
-            }
-
-            public bool OnlyOnChange
-            {
-                get => base.GetProperty<bool>(nameof(OnlyOnChange));
-                set
-                {
-                    base.UpdateProperty(nameof(OnlyOnChange), value);
-                    this.UpdateRequest?.Invoke(this);
-                }
-            }
-
-            public int FrameSkip
-            {
-                get => base.GetProperty<int>(nameof(FrameSkip));
-                set
-                {
-                    base.UpdateProperty(nameof(FrameSkip), Math.Max(value, 0));
-                    this.UpdateRequest?.Invoke(this);
-                }
-            }
-
-            public int ValueToSet
-            {
-                get => base.GetProperty<int>(nameof(ValueToSet));
-                set => base.UpdateProperty(nameof(ValueToSet), value);
-            }
-
-            public DateTime LastUpdateTime
-            {
-                get => base.GetProperty<DateTime>(nameof(LastUpdateTime));
-                private set => base.UpdateProperty(nameof(LastUpdateTime), value);
-            }
-
-            public TimeSpan LastUpdateInterval
-            {
-                get => base.GetProperty<TimeSpan>(nameof(LastUpdateInterval));
-                private set => base.UpdateProperty(nameof(LastUpdateInterval), value);
-            }
-
-            public List<SimConnectPeriod> Periods => Model.Periods;
-        }
-
-        public class SimVarEvent : NotifyPropertyChanged
-        {
-            public EventId EventId { get; internal set; }
-            public string Name { get; internal set; } = null!;
-            public DateTime LastInvoked
-            {
-                get => base.GetProperty<DateTime>(nameof(LastInvoked))!;
-                set => base.UpdateProperty(nameof(LastInvoked), value);
-            }
-        }
-
-        public EConnectionStatus ConnectionStatus
-        {
-            get => base.GetProperty<EConnectionStatus>(nameof(ConnectionStatus));
-            set => base.UpdateProperty(nameof(ConnectionStatus), value);
-        }
-
-        public BindingList<SimVarValue> Values
-        {
-            get => base.GetProperty<BindingList<SimVarValue>>(nameof(Values))!;
-            set => base.UpdateProperty(nameof(Values), value);
-        }
-
-        public BindingList<SimVarEvent> Events
-        {
-            get => base.GetProperty<BindingList<SimVarEvent>>(nameof(Events))!;
-            set => base.UpdateProperty(nameof(Events), value);
-        }
-
-        public BindingList<string> FiredEvents
-        {
-            get => base.GetProperty<BindingList<string>>(nameof(FiredEvents))!;
-            set => base.UpdateProperty(nameof(FiredEvents), value);
-        }
-
-        public Model()
-        {
-            ConnectionStatus = EConnectionStatus.DISCONNECTED;
-            Values = new();
-            Events = new();
-            FiredEvents = new();
-        }
-
-        public static List<SimConnectPeriod> Periods
-        {
-            get;
-            private set;
-        }
-
-        static Model()
-        {
-            Periods = new()
-            {
-                SimConnectPeriod.NEVER,
-                SimConnectPeriod.SECOND,
-                SimConnectPeriod.SIM_FRAME,
-                SimConnectPeriod.VISUAL_FRAME
-            };
-        }
+      CONNECTED,
+      DISCONNECTED
     }
+
+    public class SimVarValue : NotifyPropertyChanged
+    {
+      public event Action<SimVarValue>? UpdateRequest;
+      public TypeId TypeId { get; set; }
+      public RequestId RequestId { get; set; }
+
+      public string Name
+      {
+        get => base.GetProperty<string>(nameof(Name))!;
+        set => base.UpdateProperty(nameof(Name), value);
+      }
+
+      public double Value
+      {
+        get => base.GetProperty<double>(nameof(Value));
+        set
+        {
+          base.UpdateProperty(nameof(Value), value);
+          var now = DateTime.Now;
+          this.LastUpdateInterval = now - this.LastUpdateTime;
+          this.LastUpdateTime = now;
+        }
+      }
+
+      public ESimConnect.SimConnectPeriod Period
+      {
+        get => base.GetProperty<ESimConnect.SimConnectPeriod>(nameof(Period));
+        set
+        {
+          base.UpdateProperty(nameof(Period), value);
+          this.UpdateRequest?.Invoke(this);
+        }
+      }
+
+      public bool OnlyOnChange
+      {
+        get => base.GetProperty<bool>(nameof(OnlyOnChange));
+        set
+        {
+          base.UpdateProperty(nameof(OnlyOnChange), value);
+          this.UpdateRequest?.Invoke(this);
+        }
+      }
+
+      public int FrameSkip
+      {
+        get => base.GetProperty<int>(nameof(FrameSkip));
+        set
+        {
+          base.UpdateProperty(nameof(FrameSkip), Math.Max(value, 0));
+          this.UpdateRequest?.Invoke(this);
+        }
+      }
+
+      public int ValueToSet
+      {
+        get => base.GetProperty<int>(nameof(ValueToSet));
+        set => base.UpdateProperty(nameof(ValueToSet), value);
+      }
+
+      public DateTime LastUpdateTime
+      {
+        get => base.GetProperty<DateTime>(nameof(LastUpdateTime));
+        private set => base.UpdateProperty(nameof(LastUpdateTime), value);
+      }
+
+      public TimeSpan LastUpdateInterval
+      {
+        get => base.GetProperty<TimeSpan>(nameof(LastUpdateInterval));
+        private set => base.UpdateProperty(nameof(LastUpdateInterval), value);
+      }
+
+      public List<SimConnectPeriod> Periods => Model.Periods;
+
+      public string UnitName
+      {
+        get => base.GetProperty<string>(nameof(UnitName))!;
+        set
+        {
+          base.UpdateProperty(nameof(UnitName), value);
+          this.UpdateRequest?.Invoke(this);
+        }
+      }
+    }
+
+    public class SimVarEvent : NotifyPropertyChanged
+    {
+      public EventId EventId { get; internal set; }
+      public string Name { get; internal set; } = null!;
+      public DateTime LastInvoked
+      {
+        get => base.GetProperty<DateTime>(nameof(LastInvoked))!;
+        set => base.UpdateProperty(nameof(LastInvoked), value);
+      }
+    }
+
+    public EConnectionStatus ConnectionStatus
+    {
+      get => base.GetProperty<EConnectionStatus>(nameof(ConnectionStatus));
+      set => base.UpdateProperty(nameof(ConnectionStatus), value);
+    }
+
+    public BindingList<SimVarValue> Values
+    {
+      get => base.GetProperty<BindingList<SimVarValue>>(nameof(Values))!;
+      set => base.UpdateProperty(nameof(Values), value);
+    }
+
+    public BindingList<SimVarEvent> Events
+    {
+      get => base.GetProperty<BindingList<SimVarEvent>>(nameof(Events))!;
+      set => base.UpdateProperty(nameof(Events), value);
+    }
+
+    public BindingList<string> FiredEvents
+    {
+      get => base.GetProperty<BindingList<string>>(nameof(FiredEvents))!;
+      set => base.UpdateProperty(nameof(FiredEvents), value);
+    }
+
+    public Model()
+    {
+      ConnectionStatus = EConnectionStatus.DISCONNECTED;
+      Values = new();
+      Events = new();
+      FiredEvents = new();
+    }
+
+    public static List<SimConnectPeriod> Periods
+    {
+      get;
+      private set;
+    }
+
+
+    public static List<ComboBoxDataItem> SimUnits
+    {
+      get;
+      private set;
+    }
+
+    static Model()
+    {
+      Periods = new()
+      {
+        SimConnectPeriod.NEVER,
+        SimConnectPeriod.SECOND,
+        SimConnectPeriod.SIM_FRAME,
+        SimConnectPeriod.VISUAL_FRAME
+      };
+
+      SimUnits = GenerateSimUnitsComboBoxItems();
+    }
+
+    public record ComboBoxDataItem(string Label, string Value);
+    private static List<ComboBoxDataItem> GenerateSimUnitsComboBoxItems()
+    {
+      List<ComboBoxDataItem> ret = new();
+      Type t = typeof(SimUnits);
+      foreach (var cls in t.GetNestedTypes())
+      {
+        foreach (var field in cls.GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public))
+        {
+          string val = (string)field.GetValue(null)!;
+          ComboBoxDataItem cbdi = new($"{cls.Name}.{field.Name}", val);
+          ret.Add(cbdi);
+        }
+      }
+
+      ret = ret.OrderBy(q => q.Label).ToList();
+      ret.Insert(0, new ComboBoxDataItem("", "Number"));
+      return ret;
+    }
+  }
 }
